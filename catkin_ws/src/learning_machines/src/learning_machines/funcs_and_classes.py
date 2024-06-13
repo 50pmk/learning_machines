d73ea8b63efeb2a773144cade97417649efd3cf4
50pmk <59478545+50pmk@users.noreply.github.com>
2024-06-13 04:10:24 +0200

    non-working version

--- catkin_ws/src/learning_machines/src/learning_machines/funcs_and_classes.py
+++ catkin_ws/src/learning_machines/src/learning_machines/funcs_and_classes.py
@@ -10,6 +10,8 @@ from robobo_interface import (
     HardwareRobobo,
 )
 import numpy as np
+import pickle
+import csv
 import gymnasium as gym
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise
@@ -19,25 +21,51 @@ class GymEnv(gym.Env):
     # We don't need metadata, but the superclass wants to see it
     metadata = {"render_modes": ["dummy"]}
 
-    def __init__(self, render_mode=None, rob=IRobobo):
+    def __init__(self, render_mode=None, rob=IRobobo, max_steps=100):
+        super(GymEnv, self).__init__()
+
 
         assert render_mode is None
         assert rob != False 
         self.rob = rob
-        # Initialize step counter
-        self.step_count = 0
-        self.max_steps = 100  # Maximum number of steps per episode
 
-        # Define action space: two continuous values for left and right wheel speeds
-        self.action_space = gym.spaces.Box(low=np.array([-100, -100]), high=np.array([100, 100]), dtype=np.float32)
+        # Maximum number of steps per episode
+        self.max_steps = max_steps
 
-        # Define observation space: assume 4 continuous observations ranging from 0 to 10000
-        self.observation_space = gym.spaces.Box(low=0, high=20000, shape=(8,), dtype=np.float32)
+        # Define action space
+        self.action_space = gym.spaces.Box(low=np.array([0]), high=np.array([5]), dtype=np.float32)
+
+        # Define observation space: assume 8 continuous observations ranging from 0 to 10000
+        self.observation_space = gym.spaces.Box(low=0, high=200, shape=(8,), dtype=np.float32)
+
+        # Initialise for logging
+        self.log_irsdata = []
+        self.log_rewards = []
+        self.log_collision = []
+        self.log_actions = []
+        self.total_steps = 0
+        self.total_timesteps_in_learn = 500
 
     def _move(self, action):
-        self.rob.move_blocking(action[0], action[1], 100)
-        # TODO calculate rob distance
+        # self.rob.move_blocking(action[0], action[1], 100)
 
+        action = action[0]
+
+        # if action < 1:
+        #     self.rob.move_blocking(0, 100, 100)
+        # elif action < 2:
+        #     self.rob.move_blocking(50, 100, 100)
+        # elif action < 3:
+        #     self.rob.move_blocking(100, 100, 100)
+        # elif action < 4:
+        #     self.rob.move_blocking(100, 50, 100)
+        # else:
+        #     self.rob.move_blocking(100, 0, 100)
+
+        left = max(2.5 - action, 0) * 100 / 2.5
+        right = max(action - 2.5, 0) * 100 / 2.5
+        self.rob.move_blocking(left, right, 100)
+        
     def _get_obs(self):
         return self.rob.read_irs()
 
@@ -45,11 +73,23 @@ class GymEnv(gym.Env):
         return {'dummy_info': 0}
 
     def _spin_at_episode_start(self):
-        left = 0 # should take random value between no spinning and 360 degrees spinning
-        self.rob.move_blocking(left, 0, 100)
+        random_amount = np.random.randint(0, 1001)
+        self.rob.move_blocking(100, -100, random_amount)
 
-    def _reward(self):
-        pass
+    def _calculate_max_distance(self):
+        if len(self.visited_positions) < 2:
+            return 0.0
+        
+        # Calculate the pairwise differences
+        diffs = self.visited_positions[:, np.newaxis, :] - self.visited_positions[np.newaxis, :, :]
+        
+        # Compute the Euclidean distances
+        distances = np.sqrt(np.sum(diffs**2, axis=-1))
+        
+        # Find the maximum distance
+        max_distance = np.max(distances)
+        
+        return max_distance
 
     def reset(self, seed=None, options=None):
         # This line is probably needed but does nothing
@@ -64,8 +104,14 @@ class GymEnv(gym.Env):
             self.rob.talk("Put me back")
             self.rob.sleep(5)
 
-        # Variable for tracking location relative to starting location
-        self.location = {'x': 0, 'y': 0, 'degrees_rotated': 0}
+        # For maximizing explored distance reward method
+        self.visited_positions = np.array([[self.rob.get_position().x, self.rob.get_position().y]])
+        self.best_distance = 0
+
+        # self._spin_at_episode_start()
+
+        # Initialize step counter
+        self.step_count = 0
 
         observation = self._get_obs()
         info = self._get_info()
@@ -76,23 +122,67 @@ class GymEnv(gym.Env):
 
         # Take the action
         self._move(action)
-        # Increment step count
-        self.step_count += 1
+
         observation = self._get_obs()
         info = self._get_info()
 
-        # TODO
-        reward = 0 + np.sqrt(abs(self.location['x'] + abs(self.location['y'])))
-        if max(observation) > 200:
-            reward -= 10
+        # Save the new position for keeping track of how much distance the robot covered
+        position = np.array([[self.rob.get_position().x, self.rob.get_position().y]])
+        self.visited_positions = np.vstack((self.visited_positions, position))
 
-        # TODO
-        # terminated = 0
-        # TODO early termination if max distance has been found
+        # # Reward based on maximizing explored distance method
+        # new_best_distance = self._calculate_max_distance()
+        # reward = new_best_distance - self.best_distance # if improved: value, else: 0
+        # reward *= 10
+        # self.best_distance = new_best_distance
+
+        # Reward based on wheels difference
+        reward = - abs(action[0] - 2.5)
+
+        # # Reward update for collision punishment
+        # if max(observation) > 100:
+        #     reward -= 0.5
+
+        # Increment step 
+        self.step_count += 1
+        self.total_steps += 1 # Only for logging
+        
         # Determine if the episode is terminated based on the number of steps
         terminated = self.step_count >= self.max_steps
-        print(observation)
-        print(reward)
+
+        # TODO early termination if max distance has been found
+
+        # Logging
+        self.log_irsdata.append(observation)
+        self.log_rewards.append(reward)
+        self.log_collision.append(max(observation) > 100)
+        self.log_actions.append(action)
+        if self.total_steps == self.total_timesteps_in_learn:
+            with open(str(RESULT_DIR / 'data/run_2/picklec'), 'wb') as f:
+                pickle.dump({
+                    'irsdata': self.log_irsdata,
+                    'rewards': self.log_rewards,
+                    'collision': self.log_collision,
+                    'actions': self.log_actions
+                }, f)
+        # More Logging
+            with open(str(RESULT_DIR / 'data/run_2/irsc.csv'), 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['observation'])
+                writer.writerows([[obs] for obs in self.log_irsdata])
+            with open(str(RESULT_DIR / 'data/run_2/rewards1.csv'), 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['reward'])
+                writer.writerows([[reward] for reward in self.log_rewards])
+            with open(str(RESULT_DIR / 'data/run_2/collisionc.csv'), 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['collision'])
+                writer.writerows([[collision] for collision in self.log_collision])
+            with open(str(RESULT_DIR / 'data/run_2/actions1.csv'), 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(['actions'])
+                writer.writerows([[action] for action in self.log_actions])
+        # /Logging
 
         # Output index 3 has to be False, because it is a deprecated feature
         return observation, reward, terminated, False, info
@@ -211,16 +301,16 @@ def task1(rob: IRobobo):
 
     model = DDPG(
         policy = 'MlpPolicy', 
-        env = GymEnv(rob=rob), 
+        env = GymEnv(rob=rob, max_steps=100), 
         learning_rate=0.001, 
-        buffer_size=1000000, 
+        buffer_size=50000, 
         learning_starts=100, 
         batch_size=256, 
         tau=0.005, 
         gamma=0.99, 
         train_freq=1, 
         gradient_steps=1, 
-        action_noise=NormalActionNoise(mean=np.zeros(2), sigma=0.1 * np.ones(2)), 
+        action_noise=NormalActionNoise(mean=np.zeros(1), sigma=0.1 * np.ones(1)), # Change this too if the action space changes shape
         replay_buffer_class=None, 
         replay_buffer_kwargs=None, 
         optimize_memory_usage=False, 
@@ -232,6 +322,6 @@ def task1(rob: IRobobo):
         _init_setup_model=True
     )
 
-    model.learn(total_timesteps=10000, log_interval=2)
-
-    model.save(str(RESULT_DIR / 'models/ddpg_test'))
+    model.learn(total_timesteps=500, log_interval=10, progress_bar=True)
+    
+    model.save(str(RESULT_DIR / 'models/run_2c'))
